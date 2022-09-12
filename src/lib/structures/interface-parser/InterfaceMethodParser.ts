6b96fed2481bd20c951b9f1618ab219e013b5e58
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-09-11 22:50:23 -0400

    feat(interface-parser): add `parent` getter (#55)
    BREAKING CHANGE: `InterfaceMethodParser.generateFromTypeDoc()` now takes 3 parameters instead of 2.
    BREAKING CHANGE: `InterfacePropertyParser.generateFromTypeDoc()` now takes 3 parameters instead of 2.

--- src/lib/structures/interface-parser/InterfaceMethodParser.ts
+++ src/lib/structures/interface-parser/InterfaceMethodParser.ts
@@ -3,12 +3,19 @@ import { ReflectionKind } from '../../types';
 import { CommentParser, SignatureParser, SourceParser } from '../misc';
 import { Parser } from '../Parser';
 import type { ProjectParser } from '../ProjectParser';
+import type { InterfaceParser } from './InterfaceParser';
 
 /**
  * Parses data from an interface method reflection.
  * @since 3.1.0
  */
 export class InterfaceMethodParser extends Parser {
+  /**
+   * The id of the parent interface parser.
+   * @since 4.0.0
+   */
+  public readonly parentId: number;
+
   /**
    * The signature parsers of this method.
    * @since 3.1.0
@@ -18,9 +25,18 @@ export class InterfaceMethodParser extends Parser {
   public constructor(data: InterfaceMethodParser.Data, project: ProjectParser) {
     super(data, project);
 
-    const { signatures } = data;
+    const { parentId, signatures } = data;
 
+    this.parentId = parentId;
     this.signatures = signatures;
+  }
+
+  /**
+   * The parent interface parser.
+   * @since 4.0.0
+   */
+  public get parent(): InterfaceParser {
+    return this.project.find(this.parentId) as InterfaceParser;
   }
 
   /**
@@ -31,18 +47,19 @@ export class InterfaceMethodParser extends Parser {
   public toJSON(): InterfaceMethodParser.JSON {
     return {
       ...super.toJSON(),
+      parentId: this.parentId,
       signatures: this.signatures.map((signature) => signature.toJSON())
     };
   }
 
   /**
    * Generates a new {@link InterfaceMethodParser} instance from the given data.
-   * @since 1.0.0
+   * @since 3.1.0
    * @param reflection The reflection to generate the parser from.
    * @param project The project this parser belongs to.
    * @returns The generated parser.
    */
-  public static generateFromTypeDoc(reflection: JSONOutput.DeclarationReflection, project: ProjectParser): InterfaceMethodParser {
+  public static generateFromTypeDoc(reflection: JSONOutput.DeclarationReflection, parentId: number, project: ProjectParser): InterfaceMethodParser {
     const { kind, kindString = 'Unknown', id, name, comment = { summary: [] }, sources = [], signatures = [] } = reflection;
 
     if (kind !== ReflectionKind.Method) throw new Error(`Expected Method (${ReflectionKind.Method}), but received ${kindString} (${kind})`);
@@ -53,6 +70,7 @@ export class InterfaceMethodParser extends Parser {
         name,
         comment: CommentParser.generateFromTypeDoc(comment, project),
         source: sources.length ? SourceParser.generateFromTypeDoc(sources[0], project) : null,
+        parentId,
         signatures: signatures.map((signature) => SignatureParser.generateFromTypeDoc(signature, project))
       },
       project
@@ -60,7 +78,7 @@ export class InterfaceMethodParser extends Parser {
   }
 
   public static generateFromJSON(json: InterfaceMethodParser.JSON, project: ProjectParser): InterfaceMethodParser {
-    const { id, name, comment, source, signatures } = json;
+    const { id, name, comment, source, parentId, signatures } = json;
 
     return new InterfaceMethodParser(
       {
@@ -68,6 +86,7 @@ export class InterfaceMethodParser extends Parser {
         name,
         comment: CommentParser.generateFromJSON(comment, project),
         source: source ? SourceParser.generateFromJSON(source, project) : null,
+        parentId,
         signatures: signatures.map((signature) => SignatureParser.generateFromJSON(signature, project))
       },
       project
@@ -78,6 +97,12 @@ export class InterfaceMethodParser extends Parser {
 export namespace InterfaceMethodParser {
   export interface Data extends Parser.Data {
     /**
+     * The id of the parent interface parser.
+     * @since 4.0.0
+     */
+    parentId: number;
+
+    /**
      * The signature parsers of this method.
      * @since 3.1.0
      */
@@ -85,6 +110,12 @@ export namespace InterfaceMethodParser {
   }
 
   export interface JSON extends Parser.JSON {
+    /**
+     * The id of the parent interface parser.
+     * @since 4.0.0
+     */
+    parentId: number;
+
     /**
      * The signature parsers of this method in a JSON compatible format.
      * @since 3.1.0
